85be64bfc24292d35d238e98468d33e5dd959403
WXbet <57314510+WXbet@users.noreply.github.com>
2022-03-11 08:21:54 +0100

    py3 fixes

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -12,7 +12,7 @@ def localeInit():
 def _(txt):
 	t = gettext.dgettext("RemoteChannelStreamConverter", txt)
 	if t == txt:
-		print "[RemoteChannelStreamConverter] fallback to default translation for", txt
+		print("[RemoteChannelStreamConverter] fallback to default translation for", txt)
 		t = gettext.gettext(txt)
 	return t
 
